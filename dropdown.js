6e5825a5838e8890936d4de427ddf523a9fdfb87
yagmur <455198@student.fontys.nl>
2024-11-30 23:07:05 +0100

    stap 4 verder gemaakt

---
+++ dropdown.js
@@ -0,0 +1,14 @@
+
+document.addEventListener("DOMContentLoaded", () => {
+    const dropdown1 = document.querySelector("#aanvullend .dropdown-title");
+    const content1 = document.querySelector("#aanvullend .dropdown-content");
+    dropdown1.addEventListener("click", () => {
+        content1.style.display = content1.style.display === "block" ? "none" : "block";
+    });
+
+    const dropdown2 = document.querySelector("#tandarts .dropdown-title");
+    const content2 = document.querySelector("#tandarts .dropdown-content");
+    dropdown2.addEventListener("click", () => {
+        content2.style.display = content2.style.display === "block" ? "none" : "block";
+    });
+});
